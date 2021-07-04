982871a480114296f61393b96927b1fa7b93776b
Ikhlas1790 <86219834+Ikhlas1790@users.noreply.github.com>
2021-07-04 22:56:30 +0400

    Add files via upload

---
+++ Ikhlas-py1/index3.py
@@ -0,0 +1,31 @@
+import math
+
+# Relational and Equality Operators
+a=30
+b=20
+if a==b:
+  print('a is equal to b')
+else:
+  print('a and b are not equal')
+
+
+# Relational and Equality Operators
+a=30
+b=20
+if a!=b:
+  print('a is not equal to b')
+else:
+  print('a and b are equal')
+
+  # Print the square of first 10 numbers ( 1 to 10)
+for i in range(1,10):
+  print(i*i)
+
+# Print the series of first 10 even numbers ( 2 to 20)
+for i in range(20,2,-2): 
+     print(i)
+
+
+# Print the given series ( 50 , 48 ,46 ,44 ,42 , 40)
+for i in range (50,25, -2): 
+     print(i)
